81446e42d5b73d19ab08296d9aee0ca62d0fd526
Mickael Riga <mig@mypeplum.com>
2012-05-20 22:56:13 +0100

    Base of the main page, module and menu in json

--- lib/rack/populate.rb
+++ lib/rack/populate.rb
@@ -1,28 +1,42 @@
+require 'rack/golem'
+require 'json'
+
 module Rack
   module Populate
+    F = ::File
+    DIR = F.expand_path(F.dirname(__FILE__)+'/populate')
     def self.included(klass)
-      klass.extend ClassMethods
+      klass.class_eval do
+        include ::Rack::Golem
+        extend ClassMethods
+      end
     end
+
     module ClassMethods
       def new(*)
         ::Rack::Static.new(super, :urls => ["/_public"], :root => DIR)
       end 
       def config
-	@config ||= {
-	  :client_name => 'Client Name',
-	  :website_url => 'www.domain.com',
-	  :path => '/admin',
-	  :logout_path => '/admin/logout', # sometimes higher in stack
-	  :menu => ['Home', '/'],
-	}
+        @config ||= {
+          :client_name => 'Client Name',
+          :website_url => 'www.domain.com',
+          :path => '/admin',
+          :logout_path => '/admin/logout', # sometimes higher in stack
+          :menu => ['Home', '/'],
+        }
       end
     end
+
     def config
       @config ||= self.class.config.update(:path=>@r.script_name).dup
     end
     def index
-      @r.env['erb.location'] = DIR+'/../views/'
+      @r.env['erb.location'] = DIR+'/views/'
       erb :index
+    end
+    def menu
+      @res['Content-Type'] = 'application/json'
+      JSON.generate(config[:menu])
     end
   end
 end
