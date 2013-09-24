03ce04211fb1dc40b73c2daf9a703ac8274ee7bb
Mickael Riga <mig@mypeplum.com>
2013-09-24 18:47:29 +0100

    Have a delimiter in Integer#to_price_string

--- lib/populate_me/ext.rb
+++ lib/populate_me/ext.rb
@@ -4,7 +4,7 @@
 
 Integer.class_eval do
   def to_price_string
-    ("%.2f" % (self/100.0)).sub(/\.00/, '')
+    ("%.2f" % (self/100.0)).sub(/\.00/, '').reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
   end
 end
 
