5153cbcb9f5ea2826be7bcd0214fb520280e291d
Jonny <jonny@campbellhay.com>
2014-10-20 12:16:00 +0100

    created mongo test file

---
+++ test/mongo_test.rb
@@ -0,0 +1,17 @@
+require 'bacon'
+$:.unshift File.expand_path('../../lib', __FILE__)
+require 'populate_me/document'
+
+
+describe 'PopulateMe::Mongo' do
+  # PopulateMe::Mongo is the Mongo specific extention for 
+  # Document.
+  #
+  # It contains what is specific to a Mongo
+  # database.
+
+  describe 'Document methods work' do
+    
+  end
+
+end
