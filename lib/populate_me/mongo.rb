50f41c225a83cad2a60ae8cd6ac17e131a3a850e
Jonny <jonny@campbellhay.com>
2014-10-20 17:51:47 +0100

    initial connection class methods and tests

--- lib/populate_me/mongo.rb
+++ lib/populate_me/mongo.rb
@@ -12,10 +12,43 @@ module PopulateMe
     end
 
     module ClassMethods
-      # include Document::ClassMethods
+      include Document::ClassMethods
+
+
+      def collection_name name=nil
+        if name==nil
+          @collection_name ||= self.name
+        else
+          @collection_name = name
+        end
+      end
+
+      def db new_db=nil
+        if new_db==nil
+          raise "DB not set !" if !defined?(DB)
+          @db ||= DB 
+        else
+          @db = new_db
+        end
+      end
+
+      def collection
+        db[collection_name]
+      end
+
     end
 
-    
+    def perform_create
+      self.id
+    end
 
+    def perform_update
+      self.id
+    end
+
+    def perform_delete
+      self.id
+    end
+    
   end
 end
--- test/mongo_test.rb
+++ test/mongo_test.rb
@@ -1,7 +1,10 @@
 require 'bacon'
 $:.unshift File.expand_path('../../lib', __FILE__)
 require 'populate_me/mongo'
+require 'mongo'
 
+MONGO = Mongo::Connection.new
+DB    = MONGO['populate-me-test']
 
 describe 'PopulateMe::Mongo' do
   # PopulateMe::Mongo is the Mongo specific extention for 
@@ -15,10 +18,79 @@ describe 'PopulateMe::Mongo' do
     field :name
   end
 
+
+
+
   it 'Includes Document Module' do 
     CatFish.to_s.should == "Cat Fish"
     CatFish.new(name: "Fred").to_s.should == "Fred"
   end
 
 
+  describe 'Data base connection' do 
+
+    it 'Should have db set by default' do 
+      CatFish.db.should == DB
+    end
+
+    it 'Should override db' do 
+      FAKEDB = MONGO['fake-populate-me-db']
+      CatFish.db(FAKEDB)
+      CatFish.db.should == FAKEDB
+      CatFish.db(DB)
+      CatFish.db.should == DB
+    end
+
+    # it 'Should raise DB not set ! error if DB not set' do 
+
+    # end
+
+    it 'Should set db collection to class name by default' do 
+      CatFish.collection_name.should == "CatFish"
+    end
+
+    it 'Should override db collection name' do 
+      CatFish.collection_name('DogFish')
+      CatFish.collection_name.should == "DogFish"
+      CatFish.collection_name('CatFish')
+      CatFish.collection_name.should == "CatFish"
+    end 
+
+    it 'Should have collection set by default' do 
+      CatFish.collection.name.should == DB['CatFish'].name
+    end
+  end
+
+
+  # describe 'CRUD' do 
+  #   it 'Should create' do
+  #     fred = CatFish.new(id: "lll", name: "Fred")
+  #     fred.perform_create
+  #     fredo = CatFish.documents.find{|d| d['id']=='lll'}
+  #     fredo['name'].should == "Fred"
+  #   end
+
+  #   it 'Should update' do 
+  #     jason = CatFish.new(id: "vvv", name: "jason")
+  #     jason.perform_create
+  #     jason.name = "billy"
+  #     jason.perform_update
+  #     CatFish.documents.find{|d| d['id']=='vvv'}['name'].should=='billy'
+  #   end
+
+  #   it 'Should delete' do
+  #     herbert = CatFish.new id: "ccc", name: "herbert"
+  #     herbert.perform_create
+  #     CatFish.documents.find{|d| d['id']=='ccc'}.should!=nil
+  #     herbert.perform_delete
+  #     CatFish.documents.find{|d| d['id']=='ccc'}.should==nil
+  #   end
+
+  #   it 'Should not save to the document class variable' do 
+  #     CatFish.documents.should == []
+  #   end
+  # end
+
 end
+
+MONGO.drop_database['populate-me-test']
